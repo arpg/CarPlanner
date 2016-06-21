7e16d30891352b21f3311dd434d70befee2278b3
Corin Sandford <cosa0481@colorado.edu>
2016-06-21 16:15:04 -0600

    Fixed issue where gui would not continue until connected to car Node. Added negative sign to incoming y-value from car.

--- src/Localizer.cpp
+++ src/Localizer.cpp
@@ -57,7 +57,7 @@ void Localizer::TrackObject(
     pObj->m_bNodeSubscribed = false;
 
     if( !pObj->m_bNodeSubscribed ) {
-      while( m_pNode->subscribe( sUri ) == false ) { // changed if to while
+      if( m_pNode->subscribe( sUri ) == false ) { // changed if to while
         LOG(ERROR) << "Could not subscribe to " << sUri;
         this_thread::sleep_for( std::chrono::seconds(1) );
       }
@@ -217,7 +217,7 @@ void Localizer::_ThreadFunction(Localizer *pV) {
           T = Eigen::Matrix4d::Identity();
         }
 
-          Eigen::Vector3d Pos(posys.pose().data(0), posys.pose().data(1), -posys.pose().data(2));
+          Eigen::Vector3d Pos(posys.pose().data(0), -posys.pose().data(1), -posys.pose().data(2));
           Eigen::Quaterniond Quat(posys.pose().data(3), posys.pose().data(4),
               posys.pose().data(5), posys.pose().data(6));
 
