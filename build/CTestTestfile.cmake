556014424e0d90f3541c777655d4acb8a51088c9
mmiles19 <mikemiles19@gmail.com>
2019-05-14 11:34:16 -0600

    removed build dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/code/CarPlanner
-# Build directory: /home/mike/code/CarPlanner/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
